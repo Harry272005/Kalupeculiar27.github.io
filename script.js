6c0711a9f06cb39d136ab69e12f91418fdf8ebc1
kalupeculiar27 <Harrykalu187@gmail.com>
2026-05-28 17:00:29 +0100

    add all files

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let body = document.body;
-body.style.backgroundImage = "url(img/09a9b90d45160d6d0ed5494f86f3e982.jpg)";
+body.style.backgroundImage = "url()";
 body.style.backgroundSize = "cover";
 body.style.backgroundPosition = "center";
 body.style.backgroundRepeat = "no-repeat";
